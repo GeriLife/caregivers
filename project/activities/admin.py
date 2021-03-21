5191362ef180c99b34391bfca5f46b9b1d884360
Brylie Christopher Oxley <brylie@amble.fi>
2021-03-21 21:55:18 +0200

    Register activity-related models

--- project/activities/admin.py
+++ project/activities/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Activity,
+    ActivityType,
+    ParticipantRole,
+)
+
+
+@admin.register(Activity)
+class ActivityModelAdmin(admin.ModelAdmin):
+    fields = ("participant_role", "activity_type", "start", "end")
+    list_display = ("participant_role", "activity_type", "start", "end")
+    date_hierarchy = 'start'
+
+
+@admin.register(ActivityType)
+class ActivityTypeModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ParticipantRole)
+class ParticipantRoleModelAdmin(admin.ModelAdmin):
+    pass
