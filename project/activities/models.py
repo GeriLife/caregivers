c8995c7308aa5f0c8ecfb50ad2053317e5ec2c53
Brylie Christopher Oxley <brylie@amble.fi>
2021-03-21 21:07:18 +0200

    Add ActivityType

--- project/activities/models.py
+++ project/activities/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class ActivityType(models.Model):
+    """
+    A type of activity that can be conducted by a participant.
+    """
+
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
